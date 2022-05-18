747db199e889f79f06fc72fa32536b8b8a9b8dd3
onuruci <onur.yuce@metu.edu.tr>
2022-05-18 12:14:43 +0300

    contract submit to etherscan

--- contractdev/hardhat.config.js
+++ contractdev/hardhat.config.js
@@ -1,7 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
 
-const { ALCHEMY_API_KEY, ROPSTEN_PRIVATE_KEY } = process.env;
+
+const { ALCHEMY_API_KEY, ROPSTEN_PRIVATE_KEY, ETHER_SCAN_KEY } = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -13,5 +15,10 @@ module.exports = {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [`${ROPSTEN_PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHER_SCAN_KEY
   }
 };
